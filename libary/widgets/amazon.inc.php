ff6e1b4c447d6b47f42b2315bbc856d1fe63b0b1
Olaf  Urban <olafurban@olafs-air.localdomain>
2020-08-13 13:18:26 +0200

    widget

---
+++ libary/widgets/amazon.inc.php
@@ -0,0 +1,14 @@
+<?php
+$content.='	<script type="text/javascript"><!--
+				amazon_ad_tag="httpuidevde-21"; 
+				amazon_ad_width="120"; 
+				amazon_ad_height="240"; 
+				amazon_color_background="EBECDF"; 
+				amazon_color_border="ececec"; 
+				amazon_color_logo="504706"; 
+				amazon_color_link="2B5A6B"; 
+				amazon_ad_logo="hide"; 
+				amazon_ad_title="UIDEV - Shop"; //--></script>
+				<script type="text/javascript" src="http://www.assoc-amazon.de/s/asw.js"></script>';
+echo $content;
+?>
