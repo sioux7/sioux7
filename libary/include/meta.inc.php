41ea356e6c88b7bdc2739fd698569bfe93333748
Olaf  Urban <olafurban@olafs-air.localdomain>
2020-08-13 13:08:39 +0200

    include

---
+++ libary/include/meta.inc.php
@@ -0,0 +1,19 @@
+<?php
+$url=explode('/',$_GET['kategorie']);
+$anz=count($url)-1;
+if(!$_GET['kategorie']) {$_GET['kategorie'] = START;}
+$intCurrentKat = parsePageName2Id($_GET['kategorie']);
+$sql_query = "SELECT * FROM sioux7_kategorie where kategorie_id=".$intCurrentKat;
+$result = mysqli_query($GLOBALS['DB'],$sql_query);
+$row = mysqli_fetch_array($result);
+if(!$url[$anz]) {$url[$anz] = 'home';}
+echo '<title>'.DOM.' . '.$url[$anz].'</title>'.chr(10);
+echo '<meta name="description" content="'.strip_tags(trim($row['description'])).'" />'.chr(10);
+echo '<meta NAME="keywords" LANG="de" CONTENT="'.strip_tags(trim($row['keywords'])).'">'.chr(10);
+if($_GET['kategorie']) {
+	echo "<link rel=\"canonical\" href=\"".BASE."/".$_GET['kategorie'].".html\" />".chr(10);
+} else {
+	echo "<link rel=\"canonical\" href=\"".BASE."\" />".chr(10);
+}
+
+?>
