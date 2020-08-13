7fd4fe83d028d1f451f669ce2cdfb3473503f0fb
Olaf  Urban <olafurban@olafs-air.localdomain>
2020-08-13 13:10:01 +0200

    include

---
+++ libary/include/topnav.inc.php
@@ -0,0 +1,8 @@
+<?php
+$query="SELECT * FROM sioux7_kategorie WHERE dom_id=".$_SESSION['DOM']." AND lang='".$_SESSION['LANG']."' AND parent=0 AND menu='top' AND aktiv='1' ORDER BY rang";
+$result=mysqli_query($GLOBALS['DB'],$query);
+while($row=mysqli_fetch_array($result)){
+if($row['kategorie_id'] !=0 && $row['titel'] !="") {
+		echo '<a href="'.getLink($row['kategorie_id']).'">'.$row['titel'].'</a>';}
+}
+?>
