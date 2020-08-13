4f889f76ac60406f44ca93cf3d1605d929869359
Olaf  Urban <olafurban@olafs-air.localdomain>
2020-08-13 13:21:04 +0200

    widget

---
+++ libary/widgets/ticker.inc.php
@@ -0,0 +1,6 @@
+<ul class="typing">
+    <li>SIOUX7 ist ein neues CMS mit über 7 Frameworks</li>
+    <li>Entwickelt wurde es seit 2007</li>
+    <li>Open Source CMS für jeden</li>
+    <li>Version 7.0 kann nun kostenlos geladen werden</li>
+</ul>
